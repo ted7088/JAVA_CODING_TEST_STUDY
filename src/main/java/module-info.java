7e7ac2afadf43289a3888100122203f7c52d64df
ted7088 <ted3131@naver.com>
2023-05-16 21:30:37 +0900

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.coding {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.coding to javafx.fxml;
+    exports com.example.coding;
+}
