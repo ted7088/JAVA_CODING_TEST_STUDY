73cec51d9327ccae463acbc11c295c6d0f054611
ted7088 <ted3131@naver.com>
2023-05-17 11:08:07 +0900

    commit

---
+++ src/main/java/com/example/coding/Main7_2.java
@@ -0,0 +1,18 @@
+package com.example.coding;
+//이진수 출력 (재귀)
+public class Main7_2 {
+    public void DFS(int n){
+        if(n==0) return;
+        else{
+            DFS(n/2);
+            System.out.println(n%2+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Main7_2 T = new Main7_2();
+        T.DFS(11);
+
+    }
+
+}
