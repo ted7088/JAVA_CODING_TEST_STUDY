5cb717e272c5a9afd3e489485fad71a2847c04d8
ted7088 <ted3131@naver.com>
2023-05-18 10:47:43 +0900

    commit

--- src/main/java/com/example/coding/Main7_4.java
+++ src/main/java/com/example/coding/Main7_4.java
@@ -1,18 +1,19 @@
 package com.example.coding;
 //피보나치 재귀(메모이제이션)
 public class Main7_4 {
-    public int DFS(int n){
-        if(n==1) return 1;
-        else if(n==2) return 1;
-        else return DFS(n-2)+DFS(n-1);
+    static int[] fibo;
 
+    public int DFS(int n){
+        if(n==1) return fibo[n]=1;
+        else if(n==2) return fibo[n]=1;
+        else return fibo[n]=DFS(n-2)+DFS(n-1);
     }
 
     public static void main(String[] args) {
         Main7_4 T = new Main7_4();
         int n=10;
-        System.out.println(T.DFS(n));
-
-
+        fibo=new int[n+1];
+        T.DFS(n);
+        for(int i=1;i<=n;i++) System.out.print(fibo[i]+" ");
     }
 }
