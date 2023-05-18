0568b1aa983fdf6c2bc7f9afbc2638dde61042ca
ted7088 <ted3131@naver.com>
2023-05-18 10:21:16 +0900

    commit

--- src/main/java/com/example/coding/Main7_2.java
+++ src/main/java/com/example/coding/Main7_2.java
@@ -14,5 +14,4 @@ public class Main7_2 {
         T.DFS(11);
 
     }
-
 }
---
+++ src/main/java/com/example/coding/Main7_4.java
@@ -0,0 +1,13 @@
+package com.example.coding;
+//팩토리얼
+public class Main7_4 {
+    public int DFS(int n){
+
+    }
+
+    public static void main(String[] args) {
+        Main7_4 T = new Main7_4();
+
+
+    }
+}
