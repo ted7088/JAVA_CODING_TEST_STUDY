e68345c26742132a4dd903ac8adfaa6b3a6a7a3c
ted7088 <ted3131@naver.com>
2023-05-18 10:28:47 +0900

    commit

--- src/main/java/com/example/coding/Main7_4.java
+++ src/main/java/com/example/coding/Main7_4.java
@@ -1,12 +1,17 @@
 package com.example.coding;
-//팩토리얼
+//피보나치 재귀(메모이제이션)
 public class Main7_4 {
     public int DFS(int n){
+        if(n==1) return 1;
+        else if(n==2) return 1;
+        else return DFS(n-2)+DFS(n-1);
 
     }
 
     public static void main(String[] args) {
         Main7_4 T = new Main7_4();
+        int n=10;
+        System.out.println(T.DFS(n));
 
 
     }
