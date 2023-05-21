947b0c26bba9413ca09d0b1a8858a30d1a9d3dd5
ted7088 <ted3131@naver.com>
2023-05-21 09:46:42 +0900

    commit

--- src/main/java/com/example/coding/Main7_6.java
+++ src/main/java/com/example/coding/Main7_6.java
@@ -6,6 +6,11 @@ public class Main7_6 {
     static int[] ch;
     public void DFS(int L){
         if(L==n+1){
+            String tmp="";
+            for(int i=1;i<=n;i++){
+                if(ch[i]==1) tmp+=(i+" ");
+            }
+            if(tmp.length()>0) System.out.println(tmp);
 
         }else{
             ch[L]=1;
