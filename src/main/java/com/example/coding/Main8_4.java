62e4efc6c7477f873e1c633b820fd97a4dee888f
ted7088 <ted3131@naver.com>
2023-06-12 14:58:40 +0900

    commit

---
+++ src/main/java/com/example/coding/Main8_4.java
@@ -0,0 +1,35 @@
+package com.example.coding;
+
+import java.util.Scanner;
+
+//최대 점수 구하기
+public class Main8_4 {
+
+    static int[] pm;
+    static int n,m;
+    public void DFS(int L){
+        if(L==m){
+            for(int x : pm) System.out.print(x+" ");
+            System.out.println();
+        }else{
+            for(int i=1;i<=n;i++){
+                pm[L]=i;
+                DFS(L+1);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Main8_4 T = new Main8_4();
+        Scanner kb = new Scanner(System.in);
+
+        n=kb.nextInt();
+        m=kb.nextInt();
+        pm=new int[m];
+
+        T.DFS(0);
+
+
+    }
+
+}
