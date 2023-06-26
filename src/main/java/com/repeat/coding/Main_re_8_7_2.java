52f5ef4e2acd825661cd8ffdb782eadf663effa3
ted7088 <ted3131@naver.com>
2023-06-26 18:18:54 +0900

    commit

--- src/main/java/com/repeat/coding/Main_re_8_7_2.java
+++ src/main/java/com/repeat/coding/Main_re_8_7_2.java
@@ -3,22 +3,53 @@ package com.repeat.coding;
 import java.util.Scanner;
 
 public class Main_re_8_7_2 {
+    static int[] b, p, ch;
+    static int n , f;
+    boolean flag = false;
+    int[][] dy = new int[35][35];
 
-
-    int[][] dy=new int[35][35];
-    public int DFS(int n, int r){
+    public int combi(int n, int r){
         if(dy[n][r]>0) return dy[n][r];
-        if(n==r || r==0) return 1;
-        else return dy[n][r]=DFS(n-1,r-1)+DFS(n-1,r);
+        if(n==r||r==0) return 1;
+        else return dy[n][r] = combi(n-1,r-1) + combi(n-1,r);
+    }
+
+
+    public void DFS(int L, int sum){
+
+        if(flag==true) return;
+        if (L == n) {
+            if(sum==f){
+                for(int x : p) System.out.print(x+" ");
+                flag=true;
+            }
+
+        }else{
+            for(int i=1;i<=n;i++){
+                if(ch[i]==0){
+                    ch[i]=1;
+                    p[L]=i;
+                    DFS(L+1, sum+(p[L]*b[L]));
+                    ch[i] =0;
+                }
+            }
+
+        }
     }
 
     public static void main(String[] args) {
         Main_re_8_7_2 T = new Main_re_8_7_2();
         Scanner kb = new Scanner(System.in);
 
-        int n = kb.nextInt();
-        int r = kb.nextInt();
+        n=kb.nextInt();
+        f=kb.nextInt();
+        b=new int[n];
+        p=new int[n];
+        ch=new int[n+1];
 
-        System.out.println(T.DFS(n,r));
+        for(int i=0;i<n;i++){
+            b[i]=T.combi(n-1,i);
+        }
+        T.DFS(0,0);
     }
 }
