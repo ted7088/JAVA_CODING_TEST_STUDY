4fc4948291885f4c164ade49457949d4cbff3cc3
ted7088 <ted3131@naver.com>
2023-06-27 13:56:23 +0900

    commit

---
+++ src/main/java/com/example/coding/Main8_9.java
@@ -0,0 +1,45 @@
+package com.example.coding;
+
+import java.util.Scanner;
+
+//조합 구하기
+public class Main8_9 {
+
+    static int n;
+    static int m;
+    static int[] combi;
+
+    public void DFS(int L, int s){
+        if(L==m){
+            for(int x:combi) System.out.print(x + " ");
+            System.out.println();
+
+
+        }else{
+            for(int i=s; i<=n; i++){
+                combi[L]=i;
+                DFS(L+1,i+1);
+            }
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Main8_9 T = new Main8_9();
+        Scanner kb = new Scanner(System.in);
+
+        n=kb.nextInt();
+        m=kb.nextInt();
+
+        combi=new int[m];
+
+
+        T.DFS(0,1);
+
+
+    }
+
+}
