bc362ddfd8080edf061974511e7d26f1e0661617
ted7088 <ted3131@naver.com>
2023-08-11 13:36:48 +0900

    commit

---
+++ src/main/java/com/repeat/coding/Main_re_5_7.java
@@ -0,0 +1,46 @@
+package com.repeat.coding;
+
+import com.example.coding.Main5_7;
+import com.example.coding.Person;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main_re_5_7 {
+
+    public static void main(String[] args) {
+
+        Main5_7 T = new Main5_7();
+
+        Scanner kb = new Scanner(System.in);
+
+        String a = kb.next();
+
+        String b = kb.next();
+
+
+        System.out.println(T.solution(a,b));
+    }
+    public String solution(String need , String plan) {
+
+
+        String answer="YES";
+
+        Queue<Character> Q = new LinkedList<Character>();
+
+
+        for(char x : need.toCharArray()) {
+            Q.offer(x);
+        }
+        for(char x : plan.toCharArray()) {
+            if(Q.contains(x)) {
+                if(x!=Q.poll()) return "NO";
+            }
+        }
+        if(!Q.isEmpty()) return "NO";
+
+        return answer;
+
+    }
+}
