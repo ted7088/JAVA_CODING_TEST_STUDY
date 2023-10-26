68c6d4b35806b0e97e4414f6c94f0cbbe40d2ba0
master <ted3131@diquest.com>
2023-10-26 20:42:09 +0900

    commit

---
+++ src/main/java/com/repeat/coding/test4.java
@@ -0,0 +1,32 @@
+package com.repeat.coding;
+
+class Foo {
+	public int a=3;
+	public void addValue(int i) {
+		a=a+1;
+		System.out.println("Foo: "+a+"");
+	}
+	public void addFive() {
+		a+=5;
+		System.out.println("Foo: "+a+"");
+	}
+}
+class Bar extends Foo{
+	public int a=8;
+	public void addValue(double i) {
+		a=a+(int)i;
+		System.out.println("Bar: "+a+"");
+	}
+	public void addFive() {
+		a+=5;
+		System.out.println("Bar: "+a+"");
+	}
+}
+
+public class test4{
+public static void main(String[] args) {
+	Foo f= new Bar();
+	f.addValue(1);
+	f.addFive();
+}
+}
