404159e64ce6ca9d13aa965678d1a499db23c86b
master <ted3131@diquest.com>
2023-10-31 16:59:10 +0900

    commit

---
+++ src/main/java/com/example/coding/BJ_2739.java
@@ -0,0 +1,25 @@
+package com.example.coding;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BJ_2739 {
+
+    public static void main(String[] args) {
+
+        System.out.println("입력하세요:");
+        Scanner in = new Scanner(System.in);
+        int c = in.nextInt();
+
+        for (int i=2; i<10; i++){
+
+            for (int j=1; j<10; j++){
+                System.out.println(i+"x"+j+"="+i*j);
+            }
+
+            System.out.println("--------------------------");
+        }
+    }
+}
