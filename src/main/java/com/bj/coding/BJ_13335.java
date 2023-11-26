c790b45491b24a371d50be975df905782b7e5e49
master <ted3131@diquest.com>
2023-11-26 20:07:17 +0900

    commit

--- src/main/java/com/bj/coding/BJ_13335.java
+++ src/main/java/com/bj/coding/BJ_13335.java
@@ -10,36 +10,39 @@ public class BJ_13335 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken()); // 다리를 건너는 트럭의 수
-        int w= Integer.parseInt(st.nextToken()); // 다리의 길이
-        int l= Integer.parseInt(st.nextToken()); // 다리의 최대 하중
+        int w = Integer.parseInt(st.nextToken()); // 다리의 길이
+        int l = Integer.parseInt(st.nextToken()); // 다리의 최대 하중
 
         Queue<Integer> truck = new LinkedList<>();
         Queue<Integer> bridge = new LinkedList<>();
 
-        int count=0; // 시간
-        int bridgeWeight=0; //다리의 무게
+        int count = 0; // 시간
+        int bridgeWeight = 0; //다리의 무게
 
         st = new StringTokenizer(br.readLine());
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             truck.offer(Integer.parseInt(st.nextToken())); // 트럭의 무게
         }
 
-        for(int i=0; i<w; i++){
+        for (int i = 0; i < w; i++) {
             bridge.add(0); // 다리를 모두 0으로 초기화
         }
 
-        while(!bridge.isEmpty()) { // 다리위에 아무것도 없을 때까지
+        while (!bridge.isEmpty()) { // 다리위에 아무것도 없을 때까지
             count++;
-            bridgeWeight-=bridge.poll(); // 다리무게에서 내려온 트럭 무게를 뺌
-            if(!truck.isEmpty()){ // 트럭이 남아있다면
-                if(truck.peek()+bridgeWeight <= l) { // 앞의 트럭이 더해져도 다리의 최대하중보다 작다면
-                    bridgeWeight+=truck.peek();
+            bridgeWeight -= bridge.poll(); // 다리무게에서 내려온 트럭 무게를 뺌
+            if (!truck.isEmpty()) { // 트럭이 남아있다면
+                if (truck.peek() + bridgeWeight <= l) { // 앞의 트럭이 더해져도 다리의 최대하중보다 작다면
+                    bridgeWeight += truck.peek();
                     bridge.offer(truck.poll()); // 다리에 트럭 추가
-                }else{ // 최대 하중을 초과하면
+                } else { // 최대 하중을 초과하면
                     bridge.offer(0); // 다리위에 아무것도 안실림
                 }
             }
         }
-        Syste
+
+        System.out.println(count);
+
+    }
 }
