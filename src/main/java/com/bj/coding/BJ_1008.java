852127a6b49f91b822be8c955380ad47aed70b8f
master <ted3131@diquest.com>
2023-12-06 17:19:54 +0900

    commit

---
+++ src/main/java/com/bj/coding/BJ_1008.java
@@ -0,0 +1,17 @@
+package com.bj.coding;
+import java.util.Scanner;
+
+public class BJ_1008 {
+
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+
+        double a=in.nextDouble();
+        double b=in.nextDouble();
+
+        in.close();
+        System.out.print(a/b);
+
+    }
+}
