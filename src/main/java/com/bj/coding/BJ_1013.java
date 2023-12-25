ab85fcff7fd262369bdae087902c03c9be3ce87a
master <ted3131@diquest.com>
2023-12-25 23:02:25 +0900

    commit

---
+++ src/main/java/com/bj/coding/BJ_1013.java
@@ -0,0 +1,18 @@
+package com.bj.coding;
+
+import java.util.Scanner;
+
+public class BJ_1013 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String vega = "(100+1+|01)+";
+        int t = scan.nextInt();
+
+        for(int i = 0; i < t; i++) {
+            String spread = scan.next();
+            if(spread.matches(vega)) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
