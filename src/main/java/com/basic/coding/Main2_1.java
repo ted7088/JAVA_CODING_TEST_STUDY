a02bf31c77e0302ed25a5dd465140e8e3c76b6e7
master <ted3131@diquest.com>
2024-02-01 17:04:26 +0900

    commit

---
+++ src/main/java/com/basic/coding/Main2_1.java
@@ -0,0 +1,41 @@
+package com.basic.coding;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main2_1 {
+
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+
+        answer.add(arr[0]);
+
+        for(int i=1; i <n; i++){
+            if (arr[i]>arr[i-1]) answer.add(arr[i]);
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main2_1 T = new Main2_1();
+
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i =0; i<n;i++) {
+            arr[i]=kb.nextInt();
+        }
+
+        for(int x :T.solution(n,arr)){
+            System.out.print(x+ " ");
+        }
+
+    }
+
+}
