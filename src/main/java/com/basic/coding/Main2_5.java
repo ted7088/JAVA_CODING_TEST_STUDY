b04e13a2f5ba3a5c984859df9ea58d24ff1cc680
kangdaebeom <ted3131@naver.com>
2024-02-04 18:52:18 +0900

    commit

--- src/main/java/com/basic/coding/Main2_5.java
+++ src/main/java/com/basic/coding/Main2_5.java
@@ -13,7 +13,7 @@ public class Main2_5 {
         for(int i=2; i<=n;i++){
             if(ch[i]==0){
                 answer++;
-                for(int j=i; j<=n; j=j+i) ch[j]=1;
+                for(int j=i; j<=n; j=j+i) ch[j]=1; //자기의 배수에 1이라는 flag를 남김
             }
         }
 
