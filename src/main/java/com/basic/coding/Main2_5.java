312e100575b17a20300f5898ba1944fc1f3e6193
kangdaebeom <ted3131@naver.com>
2024-02-04 18:03:34 +0900

    commit

---
+++ src/main/java/com/basic/coding/Main2_5.java
@@ -0,0 +1,38 @@
+package com.basic.coding;
+
+import java.util.Scanner;
+
+public class Main2_5 {
+
+    public int solution(int n) {
+
+        int answer=0;
+
+        int[] ch = new int[n+1];
+
+        for(int i=2; i<=n;i++){
+            if(ch[i]==0){
+                answer++;
+                for(int j=i; j<=n; j=j+i) ch[j]=1;
+            }
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Main2_5 T = new Main2_5();
+
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+
+
+        System.out.println(T.solution(n));
+
+
+    }
+
+}
