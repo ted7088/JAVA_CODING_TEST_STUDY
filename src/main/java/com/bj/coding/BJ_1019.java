b0e81c5a9a44f0a4ee30e29f0bbeb032a5bfc638
kangdaebeom <ted3131@naver.com>
2024-02-09 14:53:38 +0900

    commit

--- src/main/java/com/bj/coding/BJ_1019.java
+++ src/main/java/com/bj/coding/BJ_1019.java
@@ -1,103 +1,60 @@
 package com.bj.coding;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class BJ_1019 {
-    {
-        // 숫자 카운트 배열
-        private static final int[] COUNTER = new int[10];
+    static int[] cnt;
+    static int start, end, digit;
 
-        /**
-         * 메인 함수
-         *
-         * @param args: [String[]] 매개변수
-         *
-         * @throws IOException 데이터 입출력 예외
-         */
-        public static void main (String[]args) throws IOException
-        {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-            // 마지막 페이지
-            int N = Integer.parseInt(reader.readLine());
+        digit = 1;
+        start = 1;
+        end = sc.nextInt();
 
-            solve(N);
+        cnt = new int[10];
 
-            StringBuilder builder = new StringBuilder();
-
-            for (int item : COUNTER) {
-                builder.append(item).append(" ");
+        while(start <= end) {
+            // 시작 페이지의 마지막 자리가 0이 될 때 까지 ++
+            while(start % 10 != 0 && start <= end) {
+                counting(start, digit);
+                start++;
             }
 
-            writer.write(builder.toString().trim());
-            writer.newLine();
-            writer.flush();
+            // 마지막 페이지의 마지막 자리가 9가 될 때 까지 --
+            while(end % 10 != 9 && start <= end) {
+                counting(end, digit);
+                end--;
+            }
 
-            reader.close();
-            writer.close();
+            if(start > end) break;
+
+            // 마지막 자릿수를 제거한다.
+            start /= 10;
+            end /= 10;
+
+            // start ~ end 사이의 0 ~ 9 갯수를 모두 센다.
+            // 현재 자릿수 만큼 곱해줘야한다.
+            for(int i = 0 ; i < 10 ; ++i) {
+                cnt[i] += (end - start + 1) * digit;
+            }
+
+            // 자릿수를 높인다.
+            digit *= 10;
         }
 
-        /**
-         * 알고리즘 동작 함수
-         *
-         * @param num: [int] 마지막 페이지
-         */
-        private static void solve ( int num)
-        {
-            // 시작 페이지
-            int start = 1;
-
-            // 자릿수
-            int digit = 1;
-
-            while (start <= num) {
-                // 1의 자리가 9가 될 때까지 마지막 페이지를 1씩 감소함
-                while (num % 10 != 9 && start <= num) {
-                    // 감소한 페이지 별도 카운팅
-                    count(num, digit);
-
-                    num--;
-                }
-
-                // 마지막 페이지가 시작 페이지보다 작을 경우
-                if (num < start) {
-                    // 이를 처리하지 않으면 num < 9일 경우 무한루프를 탐
-                    break;
-                }
-
-                // 1의 자리가 0이 될 때까지 시작 페이지를 1씩 증가함
-                while (start % 10 != 0 && start <= num) {
-                    // 증가한 페이지 별도 카운팅
-                    count(start, digit);
-
-                    start++;
-                }
-
-                start /= 10;
-                num /= 10;
-
-                for (int i = 0; i < 10; i++) {
-                    COUNTER[i] += (num - start + 1) * digit;
-                }
-
-                // 자릿수 증가
-                digit *= 10;
-            }
+        for(long i : cnt) {
+            System.out.print(i + " ");
         }
+    }
 
-        /**
-         * 카운트 함수
-         *
-         * @param num: [int] 대상 숫자
-         * @param digit: [int] 자릿수
-         */
-        private static void count ( int num, int digit)
-        {
-            while (num > 0) {
-                COUNTER[num % 10] += digit;
-                num /= 10;
-            }
+    private static void counting(int num, int digit) {
+        while(num > 0) {
+            cnt[num % 10] += digit;
+            num /= 10;
         }
     }
 }
+
