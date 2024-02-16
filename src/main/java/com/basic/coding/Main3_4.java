38847e2503747ed6902a7e63a2589d574fb52101
master <ted3131@diquest.com>
2024-02-16 14:01:33 +0900

    commit

--- src/main/java/com/basic/coding/Main3_4.java
+++ src/main/java/com/basic/coding/Main3_4.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Main3_4 {
     public int solution(int n, int m, int[] arr) {
 
+        int answer=0, sum=0, lt=0;
+
+        for(int rt = 0; rt<n; rt++){
+            sum+=arr[rt];
+            if(sum==m) answer++;
+            while (sum>=m){
+                sum-=arr[lt++];
+                if(sum==m) answer++;
+            }
+        }
+
 
         return answer;
 
@@ -23,6 +34,7 @@ public class Main3_4 {
         for (int i = 0; i < n; i++) {
             arr[i]=kb.nextInt();
         }
+
         System.out.print(T.solution(n,m,arr));
     }
 
