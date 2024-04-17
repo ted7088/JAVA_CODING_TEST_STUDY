5c95ac9ab58735014d47eaf23ca02ae186890377
ted7088 <ted3131@naver.com>
2024-04-17 23:04:02 +0900

    commit

--- src/main/java/com/basic/home/Main5_7.java
+++ src/main/java/com/basic/home/Main5_7.java
@@ -1,6 +1,40 @@
 package com.basic.home;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 public class Main5_7 {
 
-    sout
+    public static void main(String[] args) {
+
+        Main5_7 T = new Main5_7();
+        Scanner kb = new Scanner(System.in);
+
+        String a = kb.next();
+        String b = kb.next();
+
+        System.out.println(T.solution(a, b));
+
+    }
+
+
+    public String solution(String need, String plan) {
+
+        String answer = "YES";
+
+        Queue<Character> Q = new LinkedList<>();
+
+        for (char x : need.toCharArray()) Q.offer(x);
+        for(char x:plan.toCharArray()){
+            if (Q.contains(x)) {
+                if(x!=Q.poll()) return "NO";
+            }
+        }
+
+        if(!Q.isEmpty()) return "NO";
+
+        return answer;
+
+    }
 }
