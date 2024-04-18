43f1a82addece871577b1e5d15fdf610551ed3a6
master <ted3131@diquest.com>
2024-04-18 14:48:41 +0900

    commit

---
+++ src/main/java/com/basic/coding/Main5_8.java
@@ -0,0 +1,61 @@
+package com.basic.coding;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main5_8 {
+
+    public int solution(int n, int m, int[] arr) {
+
+        int answer = 0;
+        Queue<Person> Q = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            Q.offer(new Person(i, arr[i]));
+        }
+        while (!Q.isEmpty()) {
+            Person tmp = Q.poll();
+            for (Person x : Q) {
+                if(x.priority>tmp.priority){
+                    Q.offer(tmp);
+                    tmp=null;
+                    break;
+                }
+            }
+            if(tmp!=null){
+                answer++;
+                if(tmp.id==m) return answer;
+            }
+        }
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+
+        Main5_8 T = new Main5_8();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        int m = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
+
+    }
+
+    class Person {
+        int id;
+        int priority;
+
+        public Person(int id, int priority) {
+            this.id = id;
+            this.priority = priority;
+        }
+
+    }
+}
