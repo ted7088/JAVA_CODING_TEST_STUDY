42dc3045d2aad33d80daaba9c65ea9e8132aa7dc
ted7088 <ted3131@naver.com>
2024-05-07 23:38:46 +0900

    commit

---
+++ src/main/java/com/bj/coding/BJ_2225.java
@@ -0,0 +1,32 @@
+package com.bj.coding;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_2225 {
+    private static final int MOD = 1000000000;
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[k+1][n+1];
+        Arrays.fill(dp[1], 1);
+        for (int i = 1; i <= k; i++) dp[i][0] = 1;
+
+        for (int i = 2; i <= k; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                dp[i][j] %= MOD;
+            }
+        }
+
+        System.out.println(dp[k][n]);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new BJ_2225().solution();
+    }
+}
