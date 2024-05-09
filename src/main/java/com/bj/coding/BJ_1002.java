435d0915c55d9b75ec8ca602e002ac7ccbf1f0e6
master <ted3131@diquest.com>
2024-05-09 17:53:55 +0900

    commit

---
+++ src/main/java/com/bj/coding/BJ_1002.java
@@ -0,0 +1,14 @@
+package com.bj.coding;
+
+public class BJ_1002 {
+    public static void main(String[] args) {
+
+        double a = 0.1;
+        double b = 0.2;
+        double c = 0.3;
+
+        if(a + b == c) System.out.print("참입니다.");
+        else System.out.print("거짓입니다.");
+    }
+
+}
