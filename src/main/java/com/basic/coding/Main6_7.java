495cabcdb0469b04e2a8630921052f084a14dd8a
ted7088 <ted3131@naver.com>
2024-05-26 20:41:18 +0900

    commit

---
+++ src/main/java/com/basic/coding/Main6_7.java
@@ -0,0 +1,45 @@
+package com.basic.coding;
+
+import java.util.*;
+
+public class Main6_7 {
+
+    public static void main(String[] args) {
+        Main6_7 T = new Main6_7();
+        Scanner kb = new Scanner(System.in);
+
+        int n = kb.nextInt();
+        ArrayList<Point> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int x = kb.nextInt();
+            int y = kb.nextInt();
+
+            arr.add(new Point(x, y));
+
+        }
+        Collections.sort(arr);
+        for (Point o :
+                arr) {
+            System.out.println(o.x+" "+o.y);
+        }
+    }
+
+}
+
+class Point implements Comparable<Point> {
+
+    public int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.x == o.x) return this.y - o.y;
+        else return this.x - o.x;
+    }
+
+}
