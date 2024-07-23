0f987423cf83f356094df5d286bc952e27b1463b
master <ted3131@diquest.com>
2024-07-23 09:20:11 +0900

    commit

--- src/main/java/com/basic/coding/Main7_4.java
+++ src/main/java/com/basic/coding/Main7_4.java
@@ -1,16 +1,22 @@
 package com.basic.coding;
 
 public class Main7_4 {
+    static int[] fibo;
+
     public static void main(String[] args) {
         Main7_4 T = new Main7_4();
-        int n = 5;
+        int n = 45;
+        fibo=new int[n+1];
+        T.DFS(n);
 
-        System.out.println(T.DFS(n));
+        for(int i=1;i<=n;i++) System.out.print(fibo[i]+" ");
+//        System.out.println(T.DFS(n));
     }
     public int DFS(int n){
-        if(n==1) return 1;
-        else if (n==2) return 1;
-        else return DFS(n-2)+DFS(n-1);
+        if(fibo[n]>0) return fibo[n];
+        if(n==1) return fibo[n]=1;
+        else if (n==2) return fibo[n]=1;
+        else return fibo[n]=DFS(n-2)+DFS(n-1);
 
     }
 }
