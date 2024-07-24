b5286a6e1ce69bc8250263af0cb8cc0671e68427
master <ted3131@diquest.com>
2024-07-24 13:09:18 +0900

    commit

---
+++ src/main/java/com/basic/coding/Main7_6.java
@@ -0,0 +1,32 @@
+package com.basic.coding;
+
+public class Main7_6 {
+
+    static int n;
+    static int[] ch;
+    public void DFS(int L){
+        if(L==n+1){
+            String tmp="";
+            for(int i=1; i<=n; i++){
+                if(ch[i]==1) tmp+=(i+" ");
+            }
+            if(tmp.length()>0) System.out.println(tmp);
+
+        }else{
+            ch[L]=1;
+            DFS(L+1);
+            ch[L]=0;
+            DFS(L+1);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Main7_6 T= new Main7_6();
+        n=3;
+        ch=new int[n+1];
+        T.DFS(1);
+    }
+
+}
+
