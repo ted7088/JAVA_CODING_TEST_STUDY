7134b58da8835478dd2b064663a0ee87586a123a
master <ted3131@diquest.com>
2024-07-25 10:41:13 +0900

    commit

--- src/main/java/com/basic/coding/Main7_7.java
+++ src/main/java/com/basic/coding/Main7_7.java
@@ -17,18 +17,22 @@ public class Main7_7 {
     Node root;
 
     public void BFS(Node root){
-        Queue<Node> Q =new LinkedList<>();
+        Queue<Node> Q = new LinkedList<>();
 
         Q.offer(root);
+
         int L=0;
+
         while (!Q.isEmpty()){
             int len=Q.size();
-            System.out.println(L+" : ");
+//            System.out.println(L+" : ");
 
             for(int i=0; i<len; i++){
                 Node cur = Q.poll();
 
                 System.out.print(cur.data+" ");
+                if(cur.lt!=null) Q.offer(cur.lt);
+                if(cur.rt!=null) Q.offer(cur.rt);
 
             }
         }
@@ -36,7 +40,7 @@ public class Main7_7 {
 
     }
     public static void main(String[] args) {
-        Main7_5 tree= new Main7_5();
+        Main7_7 tree= new Main7_7();
         tree.root=new Node(1);
         tree.root.lt=new Node(2);
         tree.root.rt=new Node(3);
@@ -45,7 +49,7 @@ public class Main7_7 {
         tree.root.rt.lt=new Node(6);
         tree.root.rt.rt=new Node(7);
 
-        tree.DFS(tree.root);
+        tree.BFS(tree.root);
     }
 
 }
