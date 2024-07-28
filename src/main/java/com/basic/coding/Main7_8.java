18b2e7f778a1ac6a960b6320b5fdb1ae20ff6f7b
ted7088 <ted3131@naver.com>
2024-07-29 00:03:55 +0900

    commit

--- src/main/java/com/basic/coding/Main7_8.java
+++ src/main/java/com/basic/coding/Main7_8.java
@@ -1,7 +1,6 @@
 package com.basic.coding;
 
 
-
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -14,18 +13,38 @@ public class Main7_8 {
 
     Queue<Integer> Q = new LinkedList<>();
 
-    public int BFS(int s, int e){
+    public int BFS(int s, int e) {
+        ch = new int[1001];
+        ch[s] = 1;
+        Q.offer(s);
+        int L = 0;
 
+        while (Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                int x = Q.poll();
+                if(x==e) return L;
+                for (int j = 0; j < 3; j++) {
+                    int nx = x + dis[j];
+                    if(nx >=1 && nx<=10000 && ch[nx]==0){
+                        ch[nx]=1;
+                        Q.offer(nx);
+                    }
+                }
+            }
+            L++;
+        }
 
         return 0;
     }
+
     public static void main(String[] args) {
         Main7_8 T = new Main7_8();
 
         Scanner kb = new Scanner(System.in);
         int s = kb.nextInt();
         int e = kb.nextInt();
-        System.out.println(T.BFS(s,e));
+        System.out.println(T.BFS(s, e));
     }
 
 }
