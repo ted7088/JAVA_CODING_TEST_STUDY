2609bc35dad3fa6a9801dc48800a4d4ba6a340b5
master <ted3131@diquest.com>
2024-07-30 17:00:13 +0900

    commit

---
+++ src/main/java/com/basic/coding/Main7_9.java
@@ -0,0 +1,28 @@
+package com.basic.coding;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main7_9 {
+    Node root;
+    public int DFS(int L , Node root) {
+        if(root.lt==null && root.rt==null) return L;
+        else return Math.min(DFS(L+1, root.lt), DFS(L+1,root.rt));
+    }
+
+    public static void main(String[] args) {
+        Main7_9 tree = new Main7_9();
+
+        tree.root=new Node(1);
+        tree.root.lt=new Node(2);
+        tree.root.rt=new Node(3);
+        tree.root.lt.lt=new Node(4);
+        tree.root.lt.rt=new Node(5);
+
+        System.out.println(tree.DFS(0,tree.root));
+    }
+
+}
+
