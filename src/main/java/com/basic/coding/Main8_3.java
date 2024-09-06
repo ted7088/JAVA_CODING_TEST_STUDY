171e4309193a9c76f8e70cb560d47c414d1a217f
master <ted3131@diquest.com>
2024-09-06 16:33:08 +0900

    commit

---
+++ src/main/java/com/basic/coding/Main8_3.java
@@ -0,0 +1,44 @@
+package com.basic.coding;
+
+import java.util.Scanner;
+
+public class Main8_3 {
+
+    static int answer =Integer.MIN_VALUE,n,m;
+
+    boolean flag = false;
+    public void DFS(int L, int sum, int time, int[] ps , int[] pt){
+
+        if(time>m) return;
+
+        if(L==n){
+            answer = Math.max(answer, sum);
+        }else{
+            DFS(L+1, sum+ps[L], time+pt[L], ps,pt);
+            DFS(L+1, sum, time, ps , pt);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Main8_3 T = new Main8_3();
+
+        Scanner kb = new Scanner(System.in);
+
+        n=kb.nextInt();
+        m=kb.nextInt();
+
+        int[] a = new int[n];
+        int[] b = new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=kb.nextInt();
+            b[i]=kb.nextInt();
+        }
+
+        T.DFS(0,0,0,a,b);
+
+        System.out.println(answer);
+
+
+    }
+}
