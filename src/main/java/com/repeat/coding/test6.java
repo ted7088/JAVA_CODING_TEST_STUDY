f0c766c4907218109a765698937e95640719309c
master <ted3131@diquest.com>
2024-10-16 16:13:17 +0900

    commit

---
+++ src/main/java/com/repeat/coding/test6.java
@@ -0,0 +1,36 @@
+package com.repeat.coding;
+
+import com.example.coding.Main8_4;
+
+import java.util.Scanner;
+
+//순열 조합
+
+public class test6 {
+    static int[] pm;
+    static int n,m;
+    public void DFS(int L){
+        if(L==m){
+            for(int x : pm) System.out.print(x+" ");
+            System.out.println();
+        }else{
+            for(int i=1;i<=n;i++){
+                pm[L]=i;
+                DFS(L+1);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        Main_re_8_4  T = new Main_re_8_4 ();
+        Scanner kb = new Scanner(System.in);
+
+        n=kb.nextInt();
+        m=kb.nextInt();
+        pm=new int[m];
+
+        T.DFS(0);
+
+
+    }
+}
