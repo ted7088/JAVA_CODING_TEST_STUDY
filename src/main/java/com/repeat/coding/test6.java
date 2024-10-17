8ee0469d969524af15462edd93fa97b83cdd2fc3
master <ted3131@diquest.com>
2024-10-17 16:29:37 +0900

    commit

--- src/main/java/com/repeat/coding/test6.java
+++ src/main/java/com/repeat/coding/test6.java
@@ -7,30 +7,34 @@ import java.util.Scanner;
 //순열 조합
 
 public class test6 {
-    static int[] pm;
-    static int n,m;
-    public void DFS(int L){
-        if(L==m){
-            for(int x : pm) System.out.print(x+" ");
-            System.out.println();
-        }else{
-            for(int i=1;i<=n;i++){
-                pm[L]=i;
-                DFS(L+1);
-            }
-        }
-
-    }
     public static void main(String[] args) {
-        Main_re_8_4  T = new Main_re_8_4 ();
-        Scanner kb = new Scanner(System.in);
 
-        n=kb.nextInt();
-        m=kb.nextInt();
-        pm=new int[m];
+        int[] n= {4,3,5,2,10};
+        arr(n);
+        for(int i=0;i<5;i++) {
+            System.out.println(n[i]);
+        }
+    }
+    public static int[] arr(int[] a) {
 
-        T.DFS(0);
+        int i,j;
+        int sw,temp;
+        int n=5;
 
+        if(a[0]==0||a[0]<1)
+            return a;
 
+        for(i=0;i<n-1;i++) {
+            sw=i;
+            for(j=i+1;j<n;j++) {
+                if(a[j]>a[sw])
+                    sw=j;
+            }
+            temp=a[i];
+            a[i]=a[sw];
+            a[sw]=temp;
+
+        }
+        return a;
     }
 }
