a564936eb055726160ade8f95a3dc788b13df104
master <ted3131@diquest.com>
2024-10-18 16:12:15 +0900

    commit

--- src/main/java/com/repeat/coding/test.java
+++ src/main/java/com/repeat/coding/test.java
@@ -2,34 +2,13 @@ package com.repeat.coding;
 
 public class test {
 
-    public static void main(String[] args) {
-
-        int[] n= {4,3,5,2,10};
-        arr(n);
-        for(int i=0;i<5;i++) {
-            System.out.println(n[i]);
-        }
+    public int DFS(int n){
+        if(n==1) return 1;
+        else return n*DFS(n-1);
     }
-    public static int[] arr(int[] a) {
 
-        int i,j;
-        int sw,temp;
-        int n=5;
-
-        if(a[0]==0||a[0]<1)
-            return a;
-
-        for(i=0;i<n-1;i++) {
-            sw=i;
-            for(j=i+1;j<n;j++) {
-                if(a[j]>a[sw])
-                    sw=j;
-            }
-            temp=a[i];
-            a[i]=a[sw];
-            a[sw]=temp;
-
-        }
-        return a;
+    public static void main(String[] args) {
+        test T = new test();
+        System.out.println(T.DFS(5));
     }
 }
