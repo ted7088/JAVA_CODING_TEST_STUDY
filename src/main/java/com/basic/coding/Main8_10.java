5d3d395bc9717097b960e17fc966215eefe4c8db
master <ted3131@diquest.com>
2024-11-05 15:11:20 +0900

    commit

--- src/main/java/com/basic/coding/Main8_10.java
+++ src/main/java/com/basic/coding/Main8_10.java
@@ -4,54 +4,36 @@ import java.util.Scanner;
 
 public class Main8_10 {
 
-    static int[][] maze = new int[7][7];
-    static int[][] visited = new int[7][7];
-    static int pathCount = 0;
+    static int[] dx={-1,0,1,0};
+    static int[] dy={0,1,0,-1};
+    static int[][] board;
+    static int answer=0;
+    public static void DFS(int x, int y) {
+        if(x==7&&y==7) answer++;
+        else{
+            for(int i =0;i<4; i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(nx>=1&&nx<=7&&ny>=1&&ny<=7&&board[nx][ny]==0){
+                    board[nx][ny]=1;//갔으니까 1로
+                    DFS(nx,ny);
+                    board[nx][ny]=0; // 백트레킹 더이상 갈곳 없을 때 다시 돌아와서 하게 해줌
 
-    // 상하좌우 이동 방향 정의 (위, 아래, 왼쪽, 오른쪽)
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        // 7x7 미로 격자판 입력 받기
-        System.out.println("7x7 격자판 미로를 입력하세요 (0은 통로, 1은 벽):");
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                maze[i][j] = scanner.nextInt();
+                }
             }
         }
 
-        // (0,0)에서 출발하여 DFS 실행
-        dfs(0, 0);
-
-        // 경로 수 출력
-        System.out.println("탈출 경로의 가지수: " + pathCount);
     }
 
-    public static void dfs(int x, int y) {
-        // 도착점에 도달하면 경로 수 증가
-        if (x == 6 && y == 6) {
-            pathCount++;
-            return;
-        }
-
-        // 현재 위치 방문 표시
-        visited[x][y] = 1;
-
-        // 상하좌우로 이동
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            // 격자 범위 내에 있고, 방문하지 않은 통로(0)일 때 이동
-            if (nx >= 0 && nx < 7 && ny >= 0 && ny < 7 && maze[nx][ny] == 0 && visited[nx][ny] == 0) {
-                dfs(nx, ny);
+    public static void main(String[] args) {
+        Main8_10 T = new Main8_10();
+        Scanner kb = new Scanner(System.in);
+        board = new int[8][8];
+        for(int i=1; i<=7;i++){
+            for(int j=1; j<=7; j++){
+                board[i][j]=kb.nextInt();
             }
-        }
 
-        // 백트래킹: 방문 해제
-        visited[x][y] = 0;
+        }
     }
 }
