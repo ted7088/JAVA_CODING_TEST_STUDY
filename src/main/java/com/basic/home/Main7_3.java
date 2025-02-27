1aa4a0d4e78bd6c8fd9caa63e1078031c008316c
master <ted3131@diquest.com>
2025-02-28 08:19:17 +0900

    commit

--- src/main/java/com/basic/home/Main7_3.java
+++ src/main/java/com/basic/home/Main7_3.java
@@ -4,32 +4,34 @@ import java.util.Scanner;
 
 public class Main7_3 {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
 
-        //입력 받기
-        int N = sc.nextInt();
-        int[] arr = new int[N];
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        //버블정렬
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < N - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+        insertionSort(arr);
 
         for (int num : arr) {
-            System.out.println(num + "");
+            System.out.println(num + " ");
         }
 
+    }
 
+    private static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];//현재 삽입하는 값
+            int j = i - 1;
+
+            //key 보다 큰값들은 한칸씩 이동
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1]=arr[j];
+                j--;
+            }
+
+        }
     }
 }
