7c3b91fdcfca49e0044ebd6332cdbde414dd80c6
master <ted3131@diquest.com>
2025-03-18 09:16:20 +0900

    commit

---
+++ src/main/java/com/basic/home/Main10_5.java
@@ -0,0 +1,38 @@
+package com.basic.home;
+
+import java.util.Scanner;
+
+public class Main10_5 {
+    static int C, N , maxWeight = 0;
+    static int[] dogs;
+
+    public static void DFS(int index, int sum){
+
+        if(sum>C) return;
+        if(index==N){
+            maxWeight= Math.max(maxWeight,sum);
+        }else{
+            DFS(index+1, sum+dogs[index]);
+            DFS(index+1,sum);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        C=sc.nextInt();
+        N=sc.nextInt();
+        dogs=new int[N];
+
+        for(int i=0; i<N; i++){
+            dogs[i] = sc.nextInt();
+        }
+
+        DFS(0,0);
+        System.out.println(maxWeight);
+
+
+    }
+}
