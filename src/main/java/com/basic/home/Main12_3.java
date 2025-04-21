d449b7d546e56f68ae97c897f9d543adf2c8f1f0
master <ted3131@diquest.com>
2025-04-21 16:49:37 +0900

    commit

--- src/main/java/com/basic/home/Main12_3.java
+++ src/main/java/com/basic/home/Main12_3.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 
 public class Main12_3 {
 
-    static int[] arr;
-    static int[] dp;
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("입력할 숫자의 개수를 입력해주세요:");
+        //입력
         int n = sc.nextInt();
+        int[] arr = new int[n];
 
-        arr= new int[n];
-        dp = new int[n];
-
-
+        for (int i = 0; i < n; i++) {
+            arr[i]=sc.nextInt();
+        }
     }
 }
