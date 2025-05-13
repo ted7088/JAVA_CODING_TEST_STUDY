901639ac410748bf22b671bb13c9f6356d519d49
master <ted3131@diquest.com>
2025-05-13 16:53:02 +0900

    commit

--- src/main/java/com/basic/home/Main12_3.java
+++ src/main/java/com/basic/home/Main12_3.java
@@ -1,5 +1,7 @@
 package com.basic.home;
 
+import javafx.scene.transform.MatrixType;
+
 import java.util.Scanner;
 
 public class Main12_3 {
@@ -8,11 +10,35 @@ public class Main12_3 {
         Scanner sc = new Scanner(System.in);
 
         //입력
-        int n = sc.nextInt();
-        int[] arr = new int[n];
+        int N = sc.nextInt();
+        int[] arr = new int[N];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             arr[i]=sc.nextInt();
         }
+
+        int[] dp = new int[N];
+
+        for(int i=0; i<N; i++){
+            dp[i]=1;
+        }
+
+        // 3. 본격적인 LIS 계산 (이중 반복문)
+        for (int i = 1; i < N; i++) { // 현재 위치
+            for (int j = 0; j < i; j++) {
+                if(arr[j]<arr[i]){
+                    dp[i]=Math.max(dp[i],dp[j]+1);
+                }
+            }
+        }
+
+        int maxLen=0;
+        for (int i = 0; i < N; i++) {
+            maxLen = Math.max(maxLen, dp[i]);
+        }
+
+        System.out.println(maxLen);
+
     }
 }
+
