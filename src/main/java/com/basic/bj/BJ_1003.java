8d8186a32552d677a8e3f7852c1a0cf3c679e856
master <ted3131@diquest.com>
2025-07-08 15:46:08 +0900

    commit

---
+++ src/main/java/com/basic/bj/BJ_1003.java
@@ -0,0 +1,38 @@
+package com.basic.bj;
+import java.util.Scanner;
+
+public class BJ_1003 {
+    static Integer[][] dp = new Integer[41][2];
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        //입력값 n을 통해서 출력 값 결정
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        int T = in.nextInt();
+
+        while(T-- > 0){
+            int N = in.nextInt();
+            fibonacci(N);
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+        }
+
+    }
+
+    static Integer[] fibonacci(int N) {
+        // N에 대한 0, 1의 호출 횟수가 없을 떄(탐색하지 않은 값일 때)
+        if(dp[N][0] == null || dp[N][1] == null) {
+            // 각 N에 대한 0 호출 횟수와 1 호출 횟수를 재귀호출한다.
+            dp[N][0] = fibonacci(N - 1)[0] + fibonacci(N - 2)[0];
+            dp[N][1] = fibonacci(N - 1)[1] + fibonacci(N - 2)[1];
+        }
+        // N에 대한 0과 1, 즉 [N][0]과 [N][1] 을 담고있는 [N]을 반환한다.
+        return dp[N];
+
+    }
+}
