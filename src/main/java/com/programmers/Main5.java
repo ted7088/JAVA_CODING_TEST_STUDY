b1c9b8e1f6e4a8000ec7f361f3a1165b9f870114
master <ted3131@diquest.com>
2025-09-04 08:01:29 +0900

    commit

---
+++ src/main/java/com/programmers/Main5.java
@@ -0,0 +1,20 @@
+package com.programmers;
+
+public class Main5 {
+
+    public int[] solution(int[][] score) {
+
+        int n = score.length;
+        double[] avg = new double[n];
+        int[] rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            avg[i]=(score[i][0] + score[i][1])/2.0;
+        }
+
+
+
+
+
+    }
+}
