10f40a898a2a00bf6848e281276b853c717114b4
master <ted3131@diquest.com>
2025-09-29 16:00:12 +0900

    commit

--- src/main/java/com/programmers/Main5.java
+++ src/main/java/com/programmers/Main5.java
@@ -1,20 +1,58 @@
 package com.programmers;
 
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
 public class Main5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-    public int[] solution(int[][] score) {
+        int n = sc.nextInt();
+        int[] numbers = new int[n];
 
-        int n = score.length;
-        double[] avg = new double[n];
-        int[] rank = new int[n];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = sc.nextInt();
+        }
 
-        for (int i = 0; i < n; i++) {
-            avg[i]=(score[i][0] + score[i][1])/2.0;
+        System.out.println(solution(numbers));
+    }
+
+    public static String solution(int[] numbers) {
+        // TODO Auto-generated method stub
+
+        String[] strNumbers = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            strNumbers[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strNumbers, new Comparator<String>() {
+
+            public int compare(String o1, String o2) {
+                return (o2 + o1).compareTo(o1 + o2);
+            };
+        });
+
+
+        // 3. 예외 처리
+        //    정렬된 첫 값이 "0"이면 전체가 다 "0"인 경우이므로
+        //    결과는 그냥 "0"
+        if (strNumbers[0].equals("0")) {
+            return "0";
+        }
+
+        //문자열 합치기
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strNumbers.length; i++) {
+            sb.append(strNumbers[i]);
         }
 
 
-
-
-
+        return sb.toString();
     }
+
 }
---
+++ src/main/java/com/programmers/Main6.java
@@ -0,0 +1,12 @@
+package com.programmers;
+
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+
+public class Main6 {
+
+
+}
