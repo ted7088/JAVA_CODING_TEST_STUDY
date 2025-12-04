77e60d671aa608dac6f26f6a3a1d86f6e1b9f0eb
master <ted3131@diquest.com>
2025-12-04 09:10:28 +0900

    commit

--- src/main/java/com/programmers/Main_18.java
+++ src/main/java/com/programmers/Main_18.java
@@ -1,8 +1,6 @@
 package com.programmers;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main_18 {
 
@@ -13,28 +11,29 @@ public class Main_18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int V = sc.nextInt();
+        int N = sc.nextInt();  // 정점 개수
+        int M = sc.nextInt();  // 간선 개수
+        int V = sc.nextInt();  // 시작 정점
 
-        graph = new ArrayList[N+1];
+        graph = new ArrayList[N + 1];
 
-        for (int i = 0; i <=N; i++) {
-            graph[i]=new ArrayList<>();
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < N; i++) {
+        // 간선 입력
+        for (int i = 0; i < M; i++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-
             graph[a].add(b);
             graph[b].add(a);
-
         }
 
-        for (int i = 0; i <=N; i++) {
+        // 정점 번호가 작은 것부터 방문하려면 반드시 정렬!
+        for (int i = 1; i <= N; i++) {
             Collections.sort(graph[i]);
         }
+
         // DFS 실행
         visited = new boolean[N + 1];
         dfs(V);
@@ -47,9 +46,36 @@ public class Main_18 {
         System.out.println(sb.toString());
         sc.close();
 
+    }
 
+    //dfs
+    static void dfs(int node){
+        visited[node]=true;
+        sb.append(node).append(" ");
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
 
+    static void bfs(int start){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
 
+        while (!q.isEmpty()){
+            int now = q.poll();
+
+            sb.append(now).append(" ");
+
+            for(int next : graph[now]){
+                if(!visited[next]){
+                    visited[next] = true;
+                    q.add(next);
+                }
+            }
+        }
     }
 
 }
